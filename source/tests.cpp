fed4bfff20f77dc664a87721363e88c5b7ca3316
GottaGoGitHub <ericgrunitz@gmail.com>
2020-05-18 16:11:14 +0200

    Commit for task 1 (Vec2)

--- source/tests.cpp
+++ source/tests.cpp
@@ -1,5 +1,16 @@
 #define CATCH_CONFIG_RUNNER
 #include <catch.hpp>
+#include "vec2.hpp"
+
+TEST_CASE("Vec2 init", "[Vec2]"){
+  Vec2 v;
+  REQUIRE(v.x == 0.0);
+  REQUIRE(v.y == 0.0);
+  Vec2 z{1.5, -3.5};
+  REQUIRE(z.x == Approx(1.5));
+  REQUIRE(z.y == Approx(-3.5));
+}
+
 
 int main(int argc, char *argv[])
 {
---
+++ source/vec2.hpp
@@ -0,0 +1,9 @@
+#ifndef VEC2_HPP
+#define VEC2_HPP
+
+struct Vec2{
+    float x = 0.0;
+    float y = 0.0;
+};
+
+#endif //VEC2_HPP
